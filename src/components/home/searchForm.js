b8d6ecae5ba125906903ce0a4153ed1aa0d74504
radharukminiY <rukku558@gmail.com>
2021-02-15 23:51:14 +0530

    commit 8

--- src/components/home/searchForm.js
+++ src/components/home/searchForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 
+
 import { searchMovie, fetchMovies, setLoading } from '../../actions/searchActions';
 
 export class SearchForm extends Component {
@@ -16,25 +17,21 @@ export class SearchForm extends Component {
 
     render() {
         return (
-            <div className="jumbotron jumbotron-fluid mt-5 text-center">
-                 <div className="container">
-                    <h1 className="display-4 mb-3">
-                        <i className="fa fa-search" /> Search for a movie, TV series...
-                    </h1>
-                    <form id="searchForm" onSubmit={this.onSubmit}>
-                        <input
-                        type="text"
-                        className="form-control"
-                        name="searchText"
-                        placeholder="Search Movies, TV Series ..."
-                        onChange={this.onChange}
-                        />
-                        <button type="submit" className="btn btn-primary btn-bg mt-3">
-                            Search
-                        </button>
-                    </form>
-                 </div>
-            </div>
+          <div className="jumbotron jumbotron-fluid mt-5 text-center">
+          <div className="container">
+          <h1 className="display-4 mb-3">
+          <i className="fa fa-search" /> Search for a movie, TV series...
+          </h1>
+          <form id="searchForm" onSubmit={this.onSubmit}>
+          <input type="text" className="form-control" name="searchText"
+          placeholder="Search Movies, TV Series ..." onChange={this.onChange}/>
+          <button type="submit" className="btn btn-primary">
+          Search
+          </button>
+          </form>
+          <hr/>
+          </div>
+         </div>
         )
     }
 }
@@ -43,6 +40,4 @@ const mapStateToProps = state => ({
     text: state.movies.text
 })
 
-
 export default connect(mapStateToProps, {searchMovie, fetchMovies, setLoading})(SearchForm);
- 
